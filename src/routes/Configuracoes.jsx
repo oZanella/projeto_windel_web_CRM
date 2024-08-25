bf2bd01c840de793b2b3e814211412f0d6b17df6
oZanella <henriquezanella19@gmail.com>
2024-08-25 14:05:36 -0300

    Finalizado porjeto adicionado ajuste em configurações

--- src/routes/Configuracoes.jsx
+++ src/routes/Configuracoes.jsx
@@ -1,16 +1,25 @@
-// Verificar sobre transformar em page "Configurações"
-
 import { Grid, Box, Typography } from '@mui/material'
+import InfoIcon from '@mui/icons-material/Info'; // Importando o ícone
 import React from 'react'
 
 export const Configuracoes = () => {
   return (
-    <Box>
-    <Grid item xs>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '10rem', 
+        bgcolor: 'background.default', 
+      }}
+    >
+      <InfoIcon 
+        sx={{ fontSize: 60, mb: 2, color: 'primary.main' }} // Tamanho e cor do ícone
+      />
       <Typography variant="h6" sx={{ textAlign: 'center' }}>
-        Teste de response - Configuracoes
+        Esta página está em desenvolvimento. Volte em breve para mais informações!
       </Typography>
-    </Grid>
-  </Box>
+    </Box>
   )
 }
